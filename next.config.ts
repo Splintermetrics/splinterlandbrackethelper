4f4e87683c388b7dd87e891da5080cc8dd285f64
Splintermetrics <splintermetrics@gmail.com>
2026-03-17 18:40:46 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,3 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
+module.exports = {
+  experimental: {
+    serverActions: {},
+  },
+  images: {
+    domains: ["d36mxiodymuqjm.cloudfront.net"],
+  },
+};
